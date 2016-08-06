a96496bc53702405676d741c0f67a59b47013f99
Christopher Wilson <christopher.e.wilson@gmail.com>
2016-08-06 15:37:55 -0400

    Switching to CMU dict

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-var readline = require("readline");
+var cmudict = require('cmu-pronouncing-dictionary')
 var args = require('minimist')(process.argv.slice(2));
 
-var rl = readline.createInterface({
-	input: require('fs').createReadStream(__dirname + "/dicts/2of12.txt")
-});
+var pun_word = args._[0];
 
-var word = args._[0];
+var pronunciation = cmudict[pun_word];
 
-var pattern = new RegExp("^.{1,3}" + word.slice(1));
+if (!pronunciation) {
+	console.log("Sorry, we couldn't find \"" + pun_word + "\" in the CMU pronunciation dictionary.");
+	return;
+}
 
-var puns = [];
+var pattern = new RegExp("^.{1,3}" + pun_word.slice(1));
 
-rl.on('line', function (line) {
-	var match = pattern.exec(line);
-	if (match) {
-		var pun = line.replace(pattern, word);
-		console.log(line, pun);
+var pun_head = pronunciation.split(" ").slice(1).join(" ");
+
+var words = Object.keys(cmudict);
+
+words.forEach(function(word) {
+	var phonemes = cmudict[word];
+
+	// eliminate words that don't possess the sound of the pun word
+	var index = phonemes.indexOf(pun_head);
+
+	if (index == -1) {
+		return;
 	}
+
+	var substr = phonemes.slice(0, index + pun_head.length).split(" ");
+
+	// sound is too internal
+	if (substr.length - pun_head.split(" ").length > 2) {
+		return;
+	}
+
+	console.log(word, word.replace(pattern, pun_word));
 
 });
